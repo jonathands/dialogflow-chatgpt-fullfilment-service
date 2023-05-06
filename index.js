37316de23e875a513b351d31ea44559e417583ad
JonathanDS <jonathands@gmail.com>
2023-05-05 21:12:57 -0300

    added message length check

--- index.js
+++ index.js
@@ -10,11 +10,25 @@ app.use(express.json());
 
 app.post('/gpt-request', async (req, res) => {
   // const intentName = req.body.queryResult.intent.displayName;
-  
-  const prompt = req.body.queryResult.queryText;
+  let prompt ;
+  try {
+    prompt = req.body.queryResult.queryText || null;
+  } catch {
+    res.statusCode = 400;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('400 No message provided (invalid format)');
+  }
+
   const apiKey = process.env.API_KEY;
   const url = 'https://api.openai.com/v1/chat/completions';  
   
+  if (prompt == null || prompt.length == 0) {
+    res.statusCode = 400;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('400 No message provided');
+    return
+  }
+
   const response = await axios.post(url, {
     model: 'gpt-3.5-turbo',
     messages: [{ role: 'user', content: prompt }],
